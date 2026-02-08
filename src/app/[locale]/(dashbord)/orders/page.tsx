008f249bffb5a274b72677465fb49163429e8105
Mahmoud Hamed <mahmoud.hamed.shenawy@gmail.com>
2026-02-08 17:43:52 +0300

    implelemnt the ui of the header component , statisctisc and orderslist

--- src/app/[locale]/(dashbord)/orders/page.tsx
+++ src/app/[locale]/(dashbord)/orders/page.tsx
@@ -1,12 +1,16 @@
-"use client";
 import { useTranslations } from "next-intl";
+import OrderHeader from "./_components/order-header";
+import OrdersStatisctics from "./_components/order-statisctics";
+import OrdersList from "./_components/orders-list";
 
 export default function OrderPage() {
   const t = useTranslations();
 
   return (
-    <div>
-      <h1>{t("orderPage")}</h1>
-    </div>
+    <main className=" min-h-screen pt-12 pb-5 px-6 space-y-6 ">
+      <OrderHeader />
+      <OrdersStatisctics />
+      <OrdersList />
+    </main>
   );
 }
