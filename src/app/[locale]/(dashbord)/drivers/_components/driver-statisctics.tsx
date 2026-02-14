0d42ee4178f8438caa04206e70a31aaa49298275
Mahmoud Hamed <mahmoud.hamed.shenawy@gmail.com>
2026-02-14 14:29:41 +0300

    iplement the ui of the driver route

--- src/app/[locale]/(dashbord)/drivers/_components/driver-statisctics.tsx
+++ src/app/[locale]/(dashbord)/drivers/_components/driver-statisctics.tsx
@@ -1,16 +1,16 @@
 "use client";
 
-import { userStatistics } from "@/src/lib/constants/user-statistics";
 import { useTranslations } from "next-intl";
 import DashboardStatCard from "../../_components/dashboard-statcard";
+import { driverStatistics } from "@/src/lib/constants/drivers-satistics";
 
-export default function UsersStatistics() {
-  const t = useTranslations("userPage.statistics");
+export default function DriverStatistics() {
+  const t = useTranslations("driverPage.statistics");
 
   return (
     <section>
       <div className="grid grid-cols-1 sm:grid-cols-2 xl:grid-cols-4 gap-4">
-        {userStatistics.map((item) => (
+        {driverStatistics.map((item) => (
           <DashboardStatCard
             key={item.key}
             title={t(item.key)}
--- src/app/[locale]/(dashbord)/drivers/page.tsx
+++ src/app/[locale]/(dashbord)/drivers/page.tsx
@@ -1,9 +1,12 @@
 import React from "react";
+import DriverPageHeader from "./_components/driver-header";
+import DriverStatistics from "./_components/driver-statisctics";
 
 export default function page() {
   return (
     <main className=" min-h-screen pt-12 pb-5 px-6 space-y-6 ">
-      <h1>اهلا بكم في صفحة السائقين</h1>
+      <DriverPageHeader />
+      <DriverStatistics />
     </main>
   );
 }
