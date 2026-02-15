9f2b90f517366b84b1ad05ea5353ab153528d1cb
Mahmoud Hamed <mahmoud.hamed.shenawy@gmail.com>
2026-02-15 12:19:09 +0300

    implemnt the ui of the driver-page : header -statisctics - tables

--- src/app/[locale]/(dashbord)/drivers/page.tsx
+++ src/app/[locale]/(dashbord)/drivers/page.tsx
@@ -1,12 +1,14 @@
 import React from "react";
 import DriverPageHeader from "./_components/driver-header";
 import DriverStatistics from "./_components/driver-statisctics";
+import DriversList from "./_components/drivers-list";
 
 export default function page() {
   return (
     <main className=" min-h-screen pt-12 pb-5 px-6 space-y-6 ">
       <DriverPageHeader />
       <DriverStatistics />
+      <DriversList />
     </main>
   );
 }
