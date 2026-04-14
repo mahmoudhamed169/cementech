6bdc82bd59d73d7a42b7f95779e051a77bcf5464
Mahmoud Hamed <mahmoud.hamed.shenawy@gmail.com>
2026-04-14 10:47:32 +0300

    fix the problem of the login action

--- src/app/[locale]/(auth)/_actions/auth.actions.ts
+++ src/app/[locale]/(auth)/_actions/auth.actions.ts
@@ -1,7 +1,9 @@
 "use server";
 
+const API_URL = process.env.NEXT_PUBLIC_API_URL;
+
 export async function sendOtpAction(phone: string) {
-  const res = await fetch(`https://cementech-backend.vercel.app/phoneLogin`, {
+  const res = await fetch(`${API_URL}/phoneLogin`, {
     method: "POST",
     headers: { "Content-Type": "application/json" },
     body: JSON.stringify({ phone, role: "admin" }),
