2030c1d9439ee4e20d7fda7644d30e9922ebc131
Mahmoud Hamed <mahmoud.hamed.shenawy@gmail.com>
2026-04-15 14:47:57 +0300

    integrate the report oage with the back end

--- src/app/[locale]/(dashbord)/payments/reports/_components/commission-report-card.tsx
+++ src/app/[locale]/(dashbord)/payments/reports/_components/commission-report-card.tsx
@@ -1,22 +1,46 @@
-export function CommissionReportCard() {
+"use client";
+
+import { CurrencyIcon } from "@/src/components/shared/currency-icon";
+import { PlatformFeeMetrics } from "@/src/lib/services/payments/get-orders-report";
+import { useTranslations } from "next-intl";
+
+interface CommissionReportCardProps {
+  data: PlatformFeeMetrics;
+}
+
+export function CommissionReportCard({ data }: CommissionReportCardProps) {
+  const t = useTranslations("PaymentsPage.reports.commission");
+
   const stats = [
-    { label: "المعدل المتوسط", value: "20%", color: "text-gray-800" },
-    { label: "اجمالي العمولات", value: "$9,046", color: "text-gray-800" },
     {
-      label: "النمو مقارنة بالشهر الماضي",
-      value: "+15.3%",
+      label: t("bankFee"),
+      value: `${data.totalBankFee.toLocaleString()}`,
+      color: "text-gray-800",
+    },
+    {
+      label: t("totalCommission"),
+      value: `${data.totalCommission.toLocaleString()}`,
+      color: "text-gray-800",
+    },
+    {
+      label: t("platformFee"),
+      value: `${data.totalPlatformFee.toLocaleString()}`,
       color: "text-green-600",
     },
   ];
 
   return (
     <div className="rounded-2xl border border-green-100 bg-green-50 p-5 space-y-3">
-      <h3 className="text-base font-bold text-gray-800">تقرير العمولة</h3>
+      <h3 className="text-base font-bold text-gray-800">{t("title")}</h3>
       <div className="space-y-2">
         {stats.map((stat) => (
           <div key={stat.label} className="flex items-center justify-between">
             <span className="text-sm text-gray-500">{stat.label}</span>
-            <span className={`font-semibold ${stat.color}`}>{stat.value}</span>
+            <span
+              className={`font-semibold ${stat.color} flex  gap-2 items-center`}
+            >
+              {stat.value} <CurrencyIcon />
+            </span>
           </div>
         ))}
       </div>
--- src/app/[locale]/(dashbord)/payments/reports/page.tsx
+++ src/app/[locale]/(dashbord)/payments/reports/page.tsx
@@ -1,13 +1,20 @@
+
+
 import { RevenueReportCard } from "./_components/revenue-report-card";
 import { CommissionReportCard } from "./_components/commission-report-card";
 import { CustomReportForm } from "./_components/custom-report-form";
+import { getOrdersReport } from "@/src/lib/services/payments/get-orders-report";
 
-export default function ReportsPage() {
+export default async function ReportsPage() {
+  // جلب الداتا في السيرفر
+  const report = await getOrdersReport();
+
   return (
     <section className="space-y-6 p-6">
       <div className="grid grid-cols-2 gap-4">
-        <RevenueReportCard />
-        <CommissionReportCard />
+        {/* تمرير الداتا لكل كارد */}
+        <RevenueReportCard data={report.data.revenue_metrics} />
+        <CommissionReportCard data={report.data.platform_fee_metrics} />
       </div>
       <CustomReportForm />
     </section>
